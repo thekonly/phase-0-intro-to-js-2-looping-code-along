13004d6d7113f57bb5d9b960eee5c4cce42f1151
thekonly <joaopedro_m@icloud.com>
2021-08-17 19:03:44 -0400

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+//for (let age=30; age <40; age++) {
+//    console.log(`I'm ${age} years old. Happy birthday to me!`);
+//}
+
+function writeCards(names,event) {
+    const messages = []
+    for (let counter =0; counter < names.length; counter++)  {
+        messages.push(`Thank you, ${names[counter]}, for the wonderful ${event} gift!`);
+    } 
+    return messages;
+
+}
+
+function countDown(int) {
+    while (int >= 0) {
+        console.log(int)
+        int--;
+    }
+}
